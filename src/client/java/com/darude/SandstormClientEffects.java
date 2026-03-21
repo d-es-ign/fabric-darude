580b93d5a1d00ab742884461da4e975ea2c77eb2
Jasper van Es <jasper.vanes@awaze.com>
2026-03-21 15:42:24 +0000

    Fix sandstorm particle creation

--- src/client/java/com/darude/SandstormClientEffects.java
+++ src/client/java/com/darude/SandstormClientEffects.java
@@ -2,7 +2,6 @@ package com.darude;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
-import net.minecraft.particle.DustParticleEffect;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
@@ -10,13 +9,8 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.util.Identifier;
-import org.joml.Vector3f;
 
 public final class SandstormClientEffects {
-	private static final float SAND_R = 216.0f / 255.0f;
-	private static final float SAND_G = 196.0f / 255.0f;
-	private static final float SAND_B = 140.0f / 255.0f;
-	private static final DustParticleEffect SANDSTORM_PARTICLE = new DustParticleEffect(new Vector3f(SAND_R, SAND_G, SAND_B), 1.0f);
 	private static final TagKey<Biome> SANDSTORM_BIOMES = TagKey.of(RegistryKeys.BIOME, Identifier.of(DarudeMod.MOD_ID, "sandstorm_biomes"));
 
 	private SandstormClientEffects() {
@@ -32,7 +26,7 @@ public final class SandstormClientEffects {
 			return;
 		}
 
-		Vec3d origin = client.player.getPosition();
+		Vec3d origin = client.player.getPos();
 		for (int i = 0; i < 12; i++) {
 			double x = origin.x + (world.random.nextDouble() - 0.5) * 28.0;
 			double y = origin.y + world.random.nextDouble() * 8.0;
@@ -40,16 +34,14 @@ public final class SandstormClientEffects {
 
 			double vx = (world.random.nextDouble() - 0.5) * 0.02;
 			double vy = -0.02 - world.random.nextDouble() * 0.02;
-			double vz = (world.random.nextDouble() - 0.5) * 0.02;
-
-			world.addParticle(SANDSTORM_PARTICLE, false, false, x, y, z, vx, vy, vz);
+			world.addParticle(x, y, z, vx, vy, ParticleTypes.WHITE_ASH);
 		}
 
 		if (world.random.nextFloat() < 0.35f) {
 			double x = origin.x + (world.random.nextDouble() - 0.5) * 24.0;
 			double y = origin.y + world.random.nextDouble() * 10.0;
 			double z = origin.z + (world.random.nextDouble() - 0.5) * 24.0;
-			world.addParticle(ParticleTypes.WHITE_ASH, false, false, x, y, z, 0.0, -0.01, 0.0);
+			world.addParticle(x, y, z, 0.0, -0.01, ParticleTypes.WHITE_ASH);
 		}
 	}
 
@@ -59,7 +51,7 @@ public final class SandstormClientEffects {
 			return false;
 		}
 
-		return isSandstormActive(world, client.getCameraEntity().getPosition());
+		return isSandstormActive(world, client.getCameraEntity().getPos());
 	}
 
 	public static boolean isSandstormActive(ClientWorld world, Vec3d cameraPos) {
