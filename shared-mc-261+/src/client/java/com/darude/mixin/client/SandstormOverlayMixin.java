62f956abbd64eea3020cff5828f74f61e0c9af90
Jasper van Es <jasper.vanes@awaze.com>
2026-04-10 14:30:31 +0100

    Use DrawContext in mc261 overlay mixin

--- shared-mc-261+/src/client/java/com/darude/mixin/client/SandstormOverlayMixin.java
+++ shared-mc-261+/src/client/java/com/darude/mixin/client/SandstormOverlayMixin.java
@@ -3,7 +3,7 @@ package com.darude.mixin.client;
 import com.darude.SandstormOverlayRenderer;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.DrawContext;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,13 +11,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(targets = "net.minecraft.client.gui.Gui")
 public abstract class SandstormOverlayMixin {
-	@Inject(method = "render(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/client/DeltaTracker;)V", at = @At("TAIL"), require = 0)
-	private void darude$renderSandstormOverlayModern(GuiGraphics drawContext, DeltaTracker tickCounter, CallbackInfo ci) {
+	@Inject(method = "render(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/client/DeltaTracker;)V", at = @At("TAIL"), require = 0)
+	private void darude$renderSandstormOverlayModern(DrawContext drawContext, DeltaTracker tickCounter, CallbackInfo ci) {
 		SandstormOverlayRenderer.render(drawContext, Minecraft.getInstance());
 	}
 
-	@Inject(method = "render(Lnet/minecraft/client/gui/GuiGraphics;F)V", at = @At("TAIL"), require = 0)
-	private void darude$renderSandstormOverlayLegacy(GuiGraphics drawContext, float tickDelta, CallbackInfo ci) {
+	@Inject(method = "render(Lnet/minecraft/client/gui/DrawContext;F)V", at = @At("TAIL"), require = 0)
+	private void darude$renderSandstormOverlayLegacy(DrawContext drawContext, float tickDelta, CallbackInfo ci) {
 		SandstormOverlayRenderer.render(drawContext, Minecraft.getInstance());
 	}
 }
